54e11f9115b5c53222762cf639d63c374dba5bcc
bneayoub <bne.ayoub@gmail.com>
2024-01-31 16:30:31 +0100

    Update Flask installation and dependencies

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # install flask from pip3
-package { 'Flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+exec { 'upgrade pip':
+  command => '/usr/bin/pip3 install --upgrade pip',
+  path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+}
+
+exec { 'install flask and dependencies':
+  command => '/usr/bin/pip3 install Flask==2.1.0 werkzeug==2.0.0', # Adjust werkzeug version as necessary
+  unless  => '/usr/bin/pip3 freeze | grep Flask==2.1.0',
+  path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+  require => Exec['upgrade pip'],
+}
